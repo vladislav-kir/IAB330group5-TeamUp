9a39c75921ff556b67550d18a16ad505f481b46d
Vladislav Kireyev <vlad.kir@outlook.com>
2019-09-08 17:33:00 +1000

    Add the start of the project: base for login, menu with teams, explore, profile and notification pages. -Vlad

---
+++ TeamUp/TeamUp/Views/MyTeamsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeamUp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyTeamsPage : ContentPage
+    {
+        public MyTeamsPage()
+        {
+            InitializeComponent();
+            Title = "My Teams";
+        }
+    }
+}

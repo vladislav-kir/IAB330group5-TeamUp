7087ecc3e07d14a3ba7f7e2743e3ad3962dd0e4e
Lusanney G. Tran <quangvan.tran@connect.qut.edu.au>
2019-09-15 14:11:49 +1000

    Firestore Connection & User Model
    This commit include:
    + Firestore Connection established
    + User Model created
    + User ViewModel created with some query functions
    + Connect data to Explore page

---
+++ TeamUp/TeamUp/ViewModels/UsersViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Plugin.CloudFirestore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TeamUp.Models;
+using System.Linq;
+
+namespace TeamUp.ViewModels
+{
+    class UserViewModel
+    {
+        
+        public async Task<User> GetUser(string id)
+        {
+            var document = await CrossCloudFirestore.Current
+                                        .Instance
+                                        .GetCollection("User")
+                                        .GetDocument(id)
+                                        .GetDocumentAsync();
+
+            var user = document.ToObject<User>();
+
+            return user;
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            var query = await CrossCloudFirestore.Current
+                                     .Instance
+                                     .GetCollection("User")
+                                     .GetDocumentsAsync();
+
+            var UserList = query.ToObjects<User>().ToList();
+
+            foreach(User user in UserList)
+            {
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("name: " + user.name);
+                Console.WriteLine("phone: " + user.phone);
+
+            }
+            return UserList;
+        }
+    }
+}
